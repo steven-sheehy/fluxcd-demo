e34cea62a956775466bc5704647ce20c2708f476
Steven Sheehy <steven.sheehy@hedera.com>
2020-04-03 10:38:52 -0500

    Add log
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class DemoController {
     @GetMapping
     public String hello(@RequestParam String message) {
+        log.info("Saw hello {}", message);
         return "Hello " + message;
     }
 }
