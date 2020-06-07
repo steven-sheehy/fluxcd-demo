a8db0337d7d81de89fca5f86a898805a48f658fa
Steven Sheehy <steven.sheehy@hedera.com>
2020-06-07 10:51:25 -0500

    Add Testcontainers support
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Resource
+	private DataSource dataSource;
 
+	@Test
+	void contextLoads() throws Exception {
+		Assertions.assertNotNull(dataSource.getConnection());
+	}
 }
